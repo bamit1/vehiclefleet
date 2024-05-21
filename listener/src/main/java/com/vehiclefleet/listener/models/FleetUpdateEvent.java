db4db8852d002c05a8492e2bd2ff7bea286ba248
SaiPatanjali <patanjali1729@gmail.com>
2024-05-21 10:38:43 +0530

    Initial setup

---
+++ listener/src/main/java/com/vehiclefleet/listener/models/FleetUpdateEvent.java
@@ -0,0 +1,12 @@
+package com.vehiclefleet.listener.models;
+
+import lombok.Data;
+
+@Data
+public class FleetUpdateEvent {
+    private String vehicleId;
+    private double lat;
+    private double lng;
+    private int speed;
+    private int fuelLevel;
+}
